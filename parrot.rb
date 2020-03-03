337e8e5e36b47672cb7083e009aa3061b2bc162b
Peter Rose <peterroseq1@gmail.com>
2020-03-03 09:54:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(phrase = "Squawk")
+  puts "Are you a parrot?"
+  return "Are you a parrot?"
+end
+
+parrot
+parrot("Hello")
